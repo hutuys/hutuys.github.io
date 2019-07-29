dd6c906f6c96349bda396129d0dbff276c7b5d7a
huwenfeng <huwenfeng@smodock.com>
2019-07-29 13:03:13 +0800

    update blog

--- toGit.bat
+++ toGit.bat
@@ -3,8 +3,12 @@ git add .
 git commit -m "update blog"
 git push github dev -v
 
+rm -rf public
 hugo 
+
 cd ./public
+git remote add github git@github.com:hutuys/hutuys.github.io.git
+
 git add .
 git commit -m "update blog"
 git push github master -v -f
